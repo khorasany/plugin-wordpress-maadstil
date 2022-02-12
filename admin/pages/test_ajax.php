60a6b0a91ef5d767f0dbe4378d784fa9bf3464f9
Mohamadreza Saffar <djmkj11@gmail.com>
2022-02-12 19:45:06 +0330

    deleting unnecessary

--- admin/pages/test_ajax.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function kc_ajax_test(){
-    echo 'hello';
-    die();
-}
-add_action('wp_ajax_kc_ajax_test', 'kc_ajax_test');
-add_action('wp_ajax_nopriv_kc_ajax_test', 'kc_ajax_test');
